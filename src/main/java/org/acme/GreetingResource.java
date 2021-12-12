0e4bc22b1a5c5653faa7c655a44ddefbd7bd4733
ryo.hirano <study.hellbird@gmail.com>
2021-12-12 13:16:27 +0900

    feat:greeting service.

--- src/main/java/org/acme/GreetingResource.java
+++ src/main/java/org/acme/GreetingResource.java
@@ -2,15 +2,29 @@ package org.acme;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 @Path("/hello")
 public class GreetingResource {
 
+    private final GreetingService greetingService;
+
+    GreetingResource(GreetingService service) {
+        this.greetingService = service;
+    }
+
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
         return "Hello RESTEasy";
+    }
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    @Path("/greeting/{name}")
+    public String greeting(@PathParam("name") String name) {
+        return greetingService.greeting(name);
     }
 }
